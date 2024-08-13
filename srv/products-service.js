bed80ac4d577ab6d12ec93fc5e2fcc833fda590e
Gabriel Freire <gabriel.freire@lab2dev.com>
2024-08-13 22:14:02 +0000

    feat: update product

--- srv/products-service.js
+++ srv/products-service.js
@@ -3,27 +3,25 @@ const cds = require('@sap/cds')
 module.exports = service => {
     service.on('CREATE', 'Products', async request => {
         const { query } = request
+        console.log(query)
         await cds.run(query)
     })
 
     service.on('READ', 'Products', async request => {
         const { query } = request
+        console.log(query)
         return await cds.run(query)
     })
 
     service.on('UPDATE', 'Products', async request => {
-        /*  await srv.update(Books).set({discount:'10%'}).where({stock:{'>':111}}) */
-        const { Code, ...data } = request.data
-        const { Products } = service.entities
-        const affectedRows = await cds.update(Products).set(data).where({ Code })
-        if (affectedRows === 0) request.error(404, `Product with ID ${Code} not found`)
+        const { query } = request
+        console.log(query)
+        await cds.run(query)
     })
 
     service.on('DELETE', 'Products', async request => {
-        const { ID: Code } = request.data
-        const { Products } = service.entities
-        const affectedRows = await cds.delete(Products).where({ Code })
-        if (affectedRows === 0) request.error(404, `Product with Code ${Code} not found`)
-        return affectedRows
+        const { query } = request
+        console.log(query)
+        await cds.run(query)
     })
 }
